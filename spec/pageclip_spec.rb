2754b46d4f4c0e8fa5945cef19251ee2e35e40e7
Matt Colyer <matt@colyer.name>
2013-02-22 15:12:01 -0800

    Ensure that job defaults work as expected.

--- spec/pageclip_spec.rb
+++ spec/pageclip_spec.rb
@@ -4,24 +4,29 @@ describe 'Pageclip' do
   subject do
     Pageclip
   end
+
+  let(:api_key) { 'key' }
+  let(:url) { 'http://www.example.com' }
+  let(:secret) { 'a' }
+
   describe 'configuring' do
     it 'can set the api key' do
       subject.configure do |config|
-        config.api_key = 'key'
+        config.api_key = api_key
+        config.job_defaults = {}
       end
-      subject.configuration.api_key.should eq('key')
+      subject.configuration.api_key.should eq(api_key)
     end
   end
+
   describe 'taking a screenshot' do
     context 'with configuration' do
       before do
         subject.configure do |config|
           config.api_key = api_key
+          config.job_defaults = {}
         end
       end
-
-      let(:url) { 'http://www.example.com' }
-      let(:api_key) { 'key' }
 
       it 'can request with just a url' do
         stub = stub_request(:get, 'http://api.pageclip.io/v1/screenshots/').
@@ -32,8 +37,8 @@ describe 'Pageclip' do
 
       it 'can request with a url and options' do
         stub = stub_request(:get, 'http://api.pageclip.io/v1/screenshots/').
-          with(:query => {'url' => url, 'api_key' => api_key, 'secret' => 'a'})
-        subject.screenshot(url, :secret => 'a')
+          with(:query => {'url' => url, 'api_key' => api_key, 'secret' => secret})
+        subject.screenshot(url, :secret => secret)
         stub.should have_been_requested
       end
 
@@ -55,8 +60,28 @@ describe 'Pageclip' do
       end
     end
     context 'with default job options' do
-      it 'can request with just a url'
-      it 'can override a property'
+      before do
+        subject.configure do |config|
+          config.api_key = api_key
+          config.job_defaults = {
+            :secret => secret
+          }
+        end
+      end
+
+      it 'can request with just a url' do
+        stub = stub_request(:get, 'http://api.pageclip.io/v1/screenshots/').
+          with(:query => {'url' => url, 'api_key' => api_key, 'secret' => secret})
+        subject.screenshot(url)
+        stub.should have_been_requested
+      end
+
+      it 'can override a property' do
+        stub = stub_request(:get, 'http://api.pageclip.io/v1/screenshots/').
+          with(:query => {'url' => url, 'api_key' => api_key, 'secret' => 'b'})
+        subject.screenshot(url, :secret => 'b')
+        stub.should have_been_requested
+      end
     end
     context 'with a logger' do
       it 'can request with just a url'
