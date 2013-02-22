f595f194631c4103b07607d5924ae13ed1dc3e4c
Matt Colyer <matt@colyer.name>
2013-02-21 17:44:30 -0800

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+# Specify your gem's dependencies in pageclip-ruby.gemspec
+gemspec
---
+++ lib/pageclip-ruby.rb
@@ -0,0 +1,7 @@
+require "pageclip-ruby/version"
+
+module Pageclip
+  module Ruby
+    # Your code goes here...
+  end
+end
