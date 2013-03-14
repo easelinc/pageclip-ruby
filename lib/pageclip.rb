b46ebc1f83750e4e31c351fdf25c3d405ebd8b49
Matt Colyer <matt@colyer.name>
2013-03-14 13:56:51 -0700

    Handles Errno::ECONNREFUSED
    This now handles one more exception type and refactors the tests to be
    less repetitive and more comprehensive.

--- lib/pageclip.rb
+++ lib/pageclip.rb
@@ -46,7 +46,7 @@ module Pageclip
           time = Benchmark.realtime do
             begin
               response = get("#{@configuration.api_endpoint}/v1/screenshots/", request_options)
-            rescue EOFError, Errno::ETIMEDOUT
+            rescue EOFError, Errno::ETIMEDOUT, Errno::ECONNREFUSED
               if attempt = ( attempt || 1) and attempt <= 3
                 Kernel.sleep(attempt)
                 attempt += 1
@@ -67,7 +67,7 @@ module Pageclip
             time += Benchmark.realtime do
               begin
                 response = get(response['location'])
-              rescue EOFError, Errno::ETIMEDOUT
+              rescue EOFError, Errno::ETIMEDOUT, Errno::ECONNREFUSED
                 if attempt = ( attempt || 1) and attempt <= 3
                   Kernel.sleep(attempt)
                   attempt += 1
--- spec/pageclip_spec.rb
+++ spec/pageclip_spec.rb
@@ -58,144 +58,171 @@ describe 'Pageclip' do
         image_url.should eq('https://s3.amazonaws.com/bucket/1.png')
       end
 
-      it 'handles timeouts' do
-        screenshot = stub_request(:get, 'http://api.pageclip.io/v1/screenshots/').
-          with(:query => {'url' => url, 'api_key' => api_key}).to_raise(Timeout::Error)
+      describe 'errors' do
+        describe 'service responses on initial request' do
+          def validate_exception_from_status(status, exception)
+            screenshot = stub_request(:get, 'http://api.pageclip.io/v1/screenshots/').
+              with(:query => {'url' => url, 'api_key' => api_key}).to_return(:status => status)
 
-        expect { subject.screenshot(url) }.to raise_error(Pageclip::TimeoutError)
-        screenshot.should have_been_requested
-      end
-      it 'handles timeouts' do
-        screenshot = stub_request(:get, 'http://api.pageclip.io/v1/screenshots/').
-          with(:query => {'url' => url, 'api_key' => api_key}).to_raise(Timeout::Error)
+            expect { subject.screenshot(url) }.to raise_error(exception)
+            screenshot.should have_been_requested
+          end
 
-        expect { subject.screenshot(url) }.to raise_error(Pageclip::TimeoutError)
-        screenshot.should have_been_requested
-      end
+          it 'handles unauthorized' do
+            validate_exception_from_status(403, Pageclip::UnauthorizedError)
+          end
 
-      it 'handles unauthorized errors' do
-        screenshot = stub_request(:get, 'http://api.pageclip.io/v1/screenshots/').
-          with(:query => {'url' => url, 'api_key' => api_key}).to_return(:status => 403)
+          it 'handles rate limit' do
+            validate_exception_from_status(429, Pageclip::RateLimitedError)
+          end
 
-        expect { subject.screenshot(url) }.to raise_error(Pageclip::UnauthorizedError)
-        screenshot.should have_been_requested
-      end
+          it 'handles service unavailable errors' do
+            validate_exception_from_status(503, Pageclip::ServiceUnavailableError)
+          end
+        end
+        describe 'service responses on screenshot endpoint' do
+          def validate_exeception_from_status(status, exception)
+            screenshot = stub_request(:get, 'http://api.pageclip.io/v1/screenshots/').
+              with(:query => {'url' => url, 'api_key' => api_key}).
+              to_return(:status => 302, :headers => { :location => 'http://api.pageclip.io/v1/screenshots/1' })
+            result = stub_request(:get, 'http://api.pageclip.io/v1/screenshots/1').
+              to_return(:status => status)
 
-      it 'handles rate limit errors' do
-        screenshot = stub_request(:get, 'http://api.pageclip.io/v1/screenshots/').
-          with(:query => {'url' => url, 'api_key' => api_key}).to_return(:status => 429)
+            expect { subject.screenshot(url) }.to raise_error(exception)
+            screenshot.should have_been_requested
+          end
 
-        expect { subject.screenshot(url) }.to raise_error(Pageclip::RateLimitedError)
-        screenshot.should have_been_requested
-      end
+          it 'handles service unavailable errors on results' do
+            validate_exeception_from_status(503, Pageclip::ServiceUnavailableError)
+          end
 
-      it 'handles service unavailable errors' do
-        screenshot = stub_request(:get, 'http://api.pageclip.io/v1/screenshots/').
-          with(:query => {'url' => url, 'api_key' => api_key}).to_return(:status => 503)
+          it 'handles screenshot errors' do
+            validate_exeception_from_status(410, Pageclip::ScreenshotError)
+          end
+        end
 
-        expect { subject.screenshot(url) }.to raise_error(Pageclip::ServiceUnavailableError)
-        screenshot.should have_been_requested
-      end
+        describe 'network errors on initial request' do
+          def validate_exception_from_exception(network_exception, exception)
+            screenshot = stub_request(:get, 'http://api.pageclip.io/v1/screenshots/').
+              with(:query => {'url' => url, 'api_key' => api_key}).to_raise(network_exception)
 
-      it 'handles service unavailable errors on results' do
-        screenshot = stub_request(:get, 'http://api.pageclip.io/v1/screenshots/').
-          with(:query => {'url' => url, 'api_key' => api_key}).
-          to_return(:status => 302, :headers => { :location => 'http://api.pageclip.io/v1/screenshots/1' })
-        result = stub_request(:get, 'http://api.pageclip.io/v1/screenshots/1').
-          to_return(:status => 503)
+            expect { subject.screenshot(url) }.to raise_error(exception)
+            screenshot.should have_been_requested
+          end
 
-        expect { subject.screenshot(url) }.to raise_error(Pageclip::ServiceUnavailableError)
-        screenshot.should have_been_requested
-      end
+          it 'handles timeouts' do
+            validate_exception_from_exception(Timeout::Error, Pageclip::TimeoutError)
+          end
+        end
 
-      it 'handles screenshot errors' do
-        screenshot = stub_request(:get, 'http://api.pageclip.io/v1/screenshots/').
-          with(:query => {'url' => url, 'api_key' => api_key}).
-          to_return(:status => 302, :headers => { :location => 'http://api.pageclip.io/v1/screenshots/1' })
-        result = stub_request(:get, 'http://api.pageclip.io/v1/screenshots/1').to_return(:status => 410)
+        describe 'intermittant network errors on initial request' do
+          def validate_success_on_intermittant_exception(network_exception)
+            screenshot = stub_request(:get, 'http://api.pageclip.io/v1/screenshots/').
+              with(:query => {'url' => url, 'api_key' => api_key}).
+              to_raise(network_exception).then.to_return(:status => 302, :headers => { :location => 'http://api.pageclip.io/v1/screenshots/1' })
+            result = stub_request(:get, 'http://api.pageclip.io/v1/screenshots/1').
+              to_return(:status => 301, :headers => { :location => 'http://s3.amazonaws.com/bucket/1.png' })
+            Kernel.should_receive(:sleep).with(1)
 
-        expect { subject.screenshot(url) }.to raise_error(Pageclip::ScreenshotError)
-        screenshot.should have_been_requested
-        result.should have_been_requested
-      end
+            subject.screenshot(url).should eq('http://s3.amazonaws.com/bucket/1.png')
+            screenshot.should have_been_requested.times(2)
+            result.should have_been_requested
+          end
 
-      it 'handles fewer than 3 timeouts for screenshot request' do
-        screenshot = stub_request(:get, 'http://api.pageclip.io/v1/screenshots/').
-          with(:query => {'url' => url, 'api_key' => api_key}).
-          to_raise(Errno::ETIMEDOUT).then.to_return(:status => 302, :headers => { :location => 'http://api.pageclip.io/v1/screenshots/1' })
-        result = stub_request(:get, 'http://api.pageclip.io/v1/screenshots/1').
-          to_return(:status => 301, :headers => { :location => 'http://s3.amazonaws.com/bucket/1.png' })
-        Kernel.should_receive(:sleep).with(1)
+          it 'handles timeouts' do
+            validate_success_on_intermittant_exception(Errno::ETIMEDOUT)
+          end
 
-        subject.screenshot(url).should eq('http://s3.amazonaws.com/bucket/1.png')
-        screenshot.should have_been_requested.times(2)
-        result.should have_been_requested
-      end
+          it 'handles end of file errors' do
+            validate_success_on_intermittant_exception(EOFError)
+          end
 
-      it 'handles fewer than 3 EOF errors for screenshot request' do
-        screenshot = stub_request(:get, 'http://api.pageclip.io/v1/screenshots/').
-          with(:query => {'url' => url, 'api_key' => api_key}).
-          to_raise(EOFError).then.to_return(:status => 302, :headers => { :location => 'http://api.pageclip.io/v1/screenshots/1' })
-        result = stub_request(:get, 'http://api.pageclip.io/v1/screenshots/1').
-          to_return(:status => 301, :headers => { :location => 'http://s3.amazonaws.com/bucket/1.png' })
-        Kernel.should_receive(:sleep).with(1)
+          it 'handles connection refused errors' do
+            validate_success_on_intermittant_exception(Errno::ECONNREFUSED)
+          end
+        end
 
-        subject.screenshot(url).should eq('http://s3.amazonaws.com/bucket/1.png')
-        screenshot.should have_been_requested.times(2)
-        result.should have_been_requested
-      end
+        describe 'prolonged network errors on initial request' do
+          def validate_exception_on_prolonged_exception(network_exception, exception)
+            screenshot = stub_request(:get, 'http://api.pageclip.io/v1/screenshots/').
+              with(:query => {'url' => url, 'api_key' => api_key}).to_raise(network_exception).
+              then.to_raise(network_exception).then.to_raise(network_exception)
+            Kernel.should_receive(:sleep).with(1)
+            Kernel.should_receive(:sleep).with(2)
+            Kernel.should_receive(:sleep).with(3)
 
-      it 'handles errors on the third EOF error for screenshot request' do
-        screenshot = stub_request(:get, 'http://api.pageclip.io/v1/screenshots/').
-          with(:query => {'url' => url, 'api_key' => api_key}).to_raise(EOFError).
-          then.to_raise(EOFError).then.to_raise(EOFError)
-        Kernel.should_receive(:sleep).with(1)
-        Kernel.should_receive(:sleep).with(2)
-        Kernel.should_receive(:sleep).with(3)
+            expect { subject.screenshot(url) }.to raise_error(exception)
+            screenshot.should have_been_requested.times(4)
+          end
 
-        expect { subject.screenshot(url) }.to raise_error(Pageclip::ServiceUnavailableError)
-        screenshot.should have_been_requested.times(4)
-      end
+          it 'handles timeouts' do
+            validate_exception_on_prolonged_exception(Errno::ETIMEDOUT, Pageclip::ServiceUnavailableError)
+          end
 
-      it 'handles fewer than 3 timeouts for status request' do
-        screenshot = stub_request(:get, 'http://api.pageclip.io/v1/screenshots/').
-          with(:query => {'url' => url, 'api_key' => api_key}).
-          to_return(:status => 302, :headers => { :location => 'http://api.pageclip.io/v1/screenshots/1' })
-        result = stub_request(:get, 'http://api.pageclip.io/v1/screenshots/1').
-          to_raise(Errno::ETIMEDOUT).then.to_return(:status => 301, :headers => { :location => 'http://s3.amazonaws.com/bucket/1.png' })
-        Kernel.should_receive(:sleep).with(1)
+          it 'handles end of file errors' do
+            validate_exception_on_prolonged_exception(EOFError, Pageclip::ServiceUnavailableError)
+          end
 
-        subject.screenshot(url).should eq('http://s3.amazonaws.com/bucket/1.png')
-        screenshot.should have_been_requested
-        result.should have_been_requested.times(2)
-      end
+          it 'handles connection refused errors' do
+            validate_exception_on_prolonged_exception(Errno::ECONNREFUSED, Pageclip::ServiceUnavailableError)
+          end
+        end
 
-      it 'handles fewer than 3 EOF errors for status request' do
-        screenshot = stub_request(:get, 'http://api.pageclip.io/v1/screenshots/').
-          with(:query => {'url' => url, 'api_key' => api_key}).
-          to_return(:status => 302, :headers => { :location => 'http://api.pageclip.io/v1/screenshots/1' })
-        result = stub_request(:get, 'http://api.pageclip.io/v1/screenshots/1').
-          to_raise(EOFError).then.to_return(:status => 301, :headers => { :location => 'http://s3.amazonaws.com/bucket/1.png' })
-        Kernel.should_receive(:sleep).with(1)
+        describe 'intermittant network errors on the screenshot endpoint' do
+          def validate_success_on_intermittant_exception(network_exception)
+            screenshot = stub_request(:get, 'http://api.pageclip.io/v1/screenshots/').
+              with(:query => {'url' => url, 'api_key' => api_key}).
+              to_return(:status => 302, :headers => { :location => 'http://api.pageclip.io/v1/screenshots/1' })
+            result = stub_request(:get, 'http://api.pageclip.io/v1/screenshots/1').
+              to_raise(network_exception).then.to_return(:status => 301, :headers => { :location => 'http://s3.amazonaws.com/bucket/1.png' })
+            Kernel.should_receive(:sleep).with(1)
 
-        subject.screenshot(url).should eq('http://s3.amazonaws.com/bucket/1.png')
-        screenshot.should have_been_requested
-        result.should have_been_requested.times(2)
-      end
+            subject.screenshot(url).should eq('http://s3.amazonaws.com/bucket/1.png')
+            screenshot.should have_been_requested
+            result.should have_been_requested.times(2)
+          end
 
-      it 'handles errors on the third EOF error for status request' do
-        screenshot = stub_request(:get, 'http://api.pageclip.io/v1/screenshots/').
-          with(:query => {'url' => url, 'api_key' => api_key}).
-          to_return(:status => 302, :headers => { :location => 'http://api.pageclip.io/v1/screenshots/1' })
-        result = stub_request(:get, 'http://api.pageclip.io/v1/screenshots/1').
-          to_raise(EOFError).then.to_raise(EOFError).then.to_raise(EOFError)
-        Kernel.should_receive(:sleep).with(1)
-        Kernel.should_receive(:sleep).with(2)
-        Kernel.should_receive(:sleep).with(3)
+          it 'handles timeouts' do
+            validate_success_on_intermittant_exception(Errno::ETIMEDOUT)
+          end
 
-        expect { subject.screenshot(url) }.to raise_error(Pageclip::ServiceUnavailableError)
-        screenshot.should have_been_requested
-        result.should have_been_requested.times(4)
+          it 'handles end of file errors' do
+            validate_success_on_intermittant_exception(EOFError)
+          end
+
+          it 'handles connection refused errors' do
+            validate_success_on_intermittant_exception(Errno::ECONNREFUSED)
+          end
+        end
+
+        describe 'prolonged network errors on the screenshot endpoint' do
+          def validate_exception_on_prolonged_exception(network_exception, exception)
+            screenshot = stub_request(:get, 'http://api.pageclip.io/v1/screenshots/').
+              with(:query => {'url' => url, 'api_key' => api_key}).
+              to_return(:status => 302, :headers => { :location => 'http://api.pageclip.io/v1/screenshots/1' })
+            result = stub_request(:get, 'http://api.pageclip.io/v1/screenshots/1').
+              to_raise(network_exception).then.to_raise(network_exception).then.to_raise(network_exception)
+            Kernel.should_receive(:sleep).with(1)
+            Kernel.should_receive(:sleep).with(2)
+            Kernel.should_receive(:sleep).with(3)
+
+            expect { subject.screenshot(url) }.to raise_error(exception)
+            screenshot.should have_been_requested
+            result.should have_been_requested.times(4)
+          end
+
+          it 'handles timeouts' do
+            validate_exception_on_prolonged_exception(Errno::ETIMEDOUT, Pageclip::ServiceUnavailableError)
+          end
+
+          it 'handles end of file errors' do
+            validate_exception_on_prolonged_exception(EOFError, Pageclip::ServiceUnavailableError)
+          end
+
+          it 'handles connection refused errors' do
+            validate_exception_on_prolonged_exception(Errno::ECONNREFUSED, Pageclip::ServiceUnavailableError)
+          end
+        end
       end
     end
 
